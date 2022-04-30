c2ca8936c016cd48aeecfd881840a12af0d7f232
pis0sion <zihuang2010@126.com>
2022-04-30 18:43:41 +0800

    add file system commit  @Pis0sion

---
+++ api/app/Controller/Admin/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Hyperf\HttpServer\Annotation\Controller;
+
+/**
+ * \App\Controller\Admin\AdminController
+ */
+#[Controller]
+class AdminController
+{
+
+    public function backend2Launch()
+    {
+        return __FUNCTION__ ;
+    }
+}
